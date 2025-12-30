e9af46732ba3f97c64c0a813f5cce1a15a7f7842
Izuka Dev <rizalnida45@gmail.com>
2025-12-30 13:11:57 +0700

    Update settings.js

--- api/config/settings.js
+++ api/config/settings.js
@@ -1,5 +1,5 @@
-export const GITHUB_TOKEN = "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"
-export const GITHUB_REPO = "xrizaldev/cdn-repo"
+export const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+export const GITHUB_REPO = "IzukaDev0/Uploader-Cdn"
 export const GITHUB_BRANCH = "main"
 
-export const DEFAULT_FOLDER = "uploads"
+export const DEFAULT_FOLDER = "cdn"
